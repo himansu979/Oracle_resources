243509ef57f6152327589fcc7ac08d5e7a8271f0
Himansu Sahoo <himansusahoo@gmail.com>
2020-06-17 23:25:29 -0400

    added helloworld.java

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+package com.example.helloworld;
+
+//https://www.jetbrains.com/help/idea/creating-and-running-your-first-java-application.html
+
+//run configuration
+//name of the java file should match with the class name
+// class name should start with capital letter
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World tutorial for java ");
+        System.out.println("length of args : " + args.length);
+        System.out.println(args[0]);
+    }
+}
+
